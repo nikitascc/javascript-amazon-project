0d39a193b67db2e0d420c71626a0d310e19d4a88
nikitascc <20106941+nikitascc@users.noreply.github.com>
2025-01-21 03:40:41 +0500

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: "2",
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: "1",
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: "2",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: "1",
+    },
+  ];
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,6 +29,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -35,4 +40,9 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
+}
+
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
