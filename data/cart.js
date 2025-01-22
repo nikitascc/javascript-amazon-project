3bd8c8f22ce3aab6970d578e5d9c2e3c8270ac37
nikitascc <20106941+nikitascc@users.noreply.github.com>
2025-01-23 03:27:26 +0500

    Slightly modified version that fixes adding to cart

--- data/cart.js
+++ data/cart.js
@@ -4,16 +4,20 @@ if (!cart) {
   cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: "2",
+      quantity: 2,
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: "1",
+      quantity: 1,
     },
   ];
 }
 
-export function addToCart(productId) {
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+export function addToCart(productId, quantity) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -21,28 +25,53 @@ export function addToCart(productId) {
       matchingItem = cartItem;
     }
   });
+
   if (matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem.quantity += quantity;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1,
+      quantity: quantity,
     });
   }
+
   saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
+
   cart = newCart;
+
   saveToStorage();
 }
 
-export function saveToStorage() {
-  localStorage.setItem("cart", JSON.stringify(cart));
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,101 +1,82 @@
-import { cart, addToCart } from "../data/cart.js";
+// Optional: notice we can write imports on multiple
+// lines so the line doesn't get too long.
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = "";
+
 products.forEach((product) => {
   productsHTML += `
-          <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-          <div class="product-name limit-text-to-2-lines">${product.name}</div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
 
-          <div class="product-price">
-            $${formatCurrency(product.priceCents)}
-          </div>
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
 
-          <div class="product-spacer"></div>
+      <div class="product-spacer"></div>
 
-          <div class="added-to-cart js-added-to-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
-            product.id
-          }">
-            Add to Cart
-          </button>
-        </div>`;
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
 });
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-// used to show the proper cart quantity after the page refresh 
-updateCartQuantity();
-
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-  console.log(cartQuantity);
-  console.log(cart);
 }
 
-// used to prevent 'Added' message flickering after repeat button presses 
-let timeoutID;
+updateCartQuantity();
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
-    addToCart(productId);
-
+    const quantity = Number(
+      document.querySelector(`.js-quantity-selector-${productId}`).value
+    );
+    addToCart(productId, quantity);
     updateCartQuantity();
-
-// used to show the 'Added' message for 2 seconds
-    document
-    .querySelector(`.js-added-to-cart-${productId}`)
-    .classList.add("added-to-cart-visible");
-
-  clearTimeout(timeoutID);
-
-  timeoutID = setTimeout(() => {
-    // timeoutID = null;
-    document
-      .querySelector(`.js-added-to-cart-${productId}`)
-      .classList.remove("added-to-cart-visible");
-  }, 2000);
-
   });
 });
