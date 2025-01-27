4ffe5ae9ddd589ff46e39819ab08e24cd3354744
nikitascc <20106941+nikitascc@users.noreply.github.com>
2025-01-27 21:35:46 +0500

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -36,7 +36,7 @@ class Product {
   }
 
   extraInfoHTML() {
-    return '';
+    return "";
   }
 }
 
@@ -54,6 +54,36 @@ class Clothing extends Product {
     <a href="${this.sizeChartLink}" target="_blank">Size chart</a>`;
   }
 }
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a
+// }
+
+// console.log(object2);
+
+// function logThis() {
+//   console.log(this);
+// }
+
+// logThis();
+// logThis.call('hello');
+
+// this;
+
+// const object3 = {
+//   method: () => {
+//     console.log(this);
+//   },
+// };
+
+// object3.method();
 
 export const products = [
   {
