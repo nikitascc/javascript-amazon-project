2582254bb5e21aea9c312981645fa6cf19172758
nikitascc <20106941+nikitascc@users.noreply.github.com>
2025-01-28 22:18:40 +0500

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,46 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("Value2");
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve("value1");
+//   });
+// })
+//   .then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+//   .then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
