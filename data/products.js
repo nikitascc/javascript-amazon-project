60804dff374e677da6b2f9bb2128a21c4dfc2b7d
nikitascc <20106941+nikitascc@users.noreply.github.com>
2025-01-28 22:37:59 +0500

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import { formatCurrency } from "../scripts/utils/money.js";
 
 export function getProduct(productId) {
   let matchingProduct;
@@ -36,7 +36,7 @@ class Product {
   }
 
   extraInfoHTML() {
-    return '';
+    return "";
   }
 }
 
@@ -89,27 +89,45 @@ const object3 = {
 object3.method();
 */
 
-
-
 export let products = [];
 
-export function loadProducts(fun) {
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', () => {
-    products = JSON.parse(xhr.response).map((productDetails) => {
-      if (productDetails.type === 'clothing') {
-        return new Clothing(productDetails);
-      }
-      return new Product(productDetails);
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+      console.log("load products");
     });
-    console.log('load products');
-    fun();
-  })
-  xhr.open('GET', 'https://supersimplebackend.dev/products');
-  xhr.send();
-  
+
+  return promise;
 }
 
+// loadProductsFetch().then(() => {
+//   console.log("next step");
+// });
+
+// export function loadProducts(fun) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.addEventListener("load", () => {
+//     products = JSON.parse(xhr.response).map((productDetails) => {
+//       if (productDetails.type === "clothing") {
+//         return new Clothing(productDetails);
+//       }
+//       return new Product(productDetails);
+//     });
+//     console.log("load products");
+//     fun();
+//   });
+//   xhr.open("GET", "https://supersimplebackend.dev/products");
+//   xhr.send();
+// }
 
 // export const products = [
 //   {
